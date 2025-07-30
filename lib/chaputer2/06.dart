f064de263ce64b530fdc13dacbc56cb734f7cf7b
hikida <ponponda0103@gmail.com>
2025-07-30 19:24:42 +0900

    Changes

---
+++ lib/chaputer2/06.dart
@@ -0,0 +1,20 @@
+const int number = 150;
+// words = myFunction();
+
+void main()
+{
+  String moji = '文字列は基本的にシングルクォーテーション';
+  // const List<>fruits=['りんご','ごりら']; 左<>の中を空にはできない。型予想できないならdynamicをいれよう
+
+  // String moji = '文字列は基本的にシングルクォーテーション';
+  // const List<dynamic>fruits=['りんご','ごりら',moji];
+  // 上記でエラーになる理由はconstで宣言しているリストに文字列を入れている為(定数ではない)
+  // つまり修正するには以下のようにfinalを使うか、Stringもconstを使う
+
+  // finalは実行時に一度だけ、一般的にはユーザ入力や日付
+  // constはコンパイル時に確定、一般的には数字や固定テキストを入れる
+  final List<dynamic> fruits = ['りんご','ごりら',moji];
+  print(fruits);
+
+
+}
